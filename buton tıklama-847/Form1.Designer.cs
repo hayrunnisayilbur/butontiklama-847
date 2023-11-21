645b63d2950005c48c1a37c9ebc49ac292336dbe
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-21 10:13:57 +0300

    Add files via upload

---
+++ buton tıklama-847/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace buton_tıklama_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.lbAdlar = new System.Windows.Forms.ListBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(111, 85);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(100, 20);
+            this.txtAranan.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(231, 85);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // lbAdlar
+            // 
+            this.lbAdlar.FormattingEnabled = true;
+            this.lbAdlar.Items.AddRange(new object[] {
+            "ömer",
+            "hayrunnisa",
+            "eymen",
+            "kübra",
+            "sukeyna",
+            "rabia",
+            "arda",
+            "hayrunnisa",
+            "deniz",
+            "ilayda",
+            "hayrunnisa",
+            "mehmet",
+            "efe",
+            "ömer"});
+            this.lbAdlar.Location = new System.Drawing.Point(125, 114);
+            this.lbAdlar.Name = "lbAdlar";
+            this.lbAdlar.Size = new System.Drawing.Size(147, 238);
+            this.lbAdlar.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(111, 385);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(52, 13);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "... kişi var";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lbAdlar);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtAranan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAranan;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.ListBox lbAdlar;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ buton tıklama-847/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buton_tıklama_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+            int sayac = 0;
+
+            for (int  i= 0; i < lbAdlar.Items.Count; i++)
+            {
+                string aranan = lbAdlar.Items[i].ToString();
+                if (aranan == txtAranan.Text)
+                {
+                    durum = true;
+                    sayac++;
+                }
+            }
+            if (durum)
+            {
+                lblSonuc.Text = "aradığınız kişiden" + sayac + " tane var.";
+            }
+            else
+            {
+                lblSonuc.Text = "aradığınız kişi yok.";
+            }
+        }   
+    }
+}
